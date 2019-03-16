ac7b286b9f5af9dbf8341145cac308b266bcea21
Alex <alexandra.hoffer@gmail.com>
2019-03-16 02:20:04 +0000

    adding first project

---
+++ app.js
@@ -0,0 +1,41 @@
+var express = require("express");
+var app = express();
+
+app.get("/", function(req, res) {
+    res.send("Hi there, welcome to my assignment!");
+});
+
+app.get("/speak/:animal", function(req, res) {
+    var animal = req.params.animal;
+    var noise;
+    switch(animal) {
+        case "dog":
+            noise = "woof";
+            break;
+        case "cow":
+            noise = "moo";
+            break;
+        case "pig":
+            noise = "oink";
+            break;
+        default:
+        break;
+    }
+    res.send("The " + animal + " says " + noise);
+});
+
+app.get("/repeat/:string/:count", function(req, res) {
+    var string = req.params.string;
+    var count = parseInt(req.params.count, 10);
+    var response = "";
+    for (var i = 0; i < count; i++) {
+        response += string + " ";
+        
+    }
+    res.send(response);
+})
+
+app.get("*", function(req, res) {
+    res.send("What are you doing with your life?");
+});
+app.listen(process.env.PORT, process.env.IP);
