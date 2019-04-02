4a626c5a3a63151adf070bcad9607c4c4fb987e8
Alex <alexandra.hoffer@gmail.com>
2019-04-02 05:57:14 +0000

    lesson 301: Yelpcamp with mongoose

---
+++ yelpcamp/index.js
@@ -0,0 +1,38 @@
+var express = require("express");
+var app = express();
+var request = require("request");
+app.set("view engine", "ejs");
+var bodyParser = require("body-parser");
+var mongoose = require("mongoose");
+mongoose.connect("mongodb://localhost/yelp_camp")
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var campgroundSchema = new mongoose.Schema({
+    name: String,
+    img_url: String,
+    description: String
+});
+
+var campground = mongoose.model("Campground", campgroundSchema);
+
+app.get("/campgrounds", function(req, res) {
+    campground.find({}, function(error, results) {
+            res.render("campgrounds", {campgrounds:results})
+    });
+});
+
+
+app.post("/campgrounds", function(req, res) {
+    var name = req.body.name;
+    var url = req.body.url;
+    var description = req.body.description;
+    campground.create({name:name, img_url:url, description:description});
+    res.redirect("/campgrounds");
+    });
+
+app.get("/campgrounds/new", function(req, res) {
+    res.render("form");
+});
+
+
+app.listen(process.env.PORT, process.env.IP);
