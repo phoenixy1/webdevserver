d9055baf5e42fe7d77a393d8efc62350917e42b7
Alex <alexandra.hoffer@gmail.com>
2019-04-22 06:19:57 +0000

    complete sample authentication app

--- secret_app/index.js
+++ secret_app/index.js
@@ -12,16 +12,18 @@ require('dotenv').config();
 var app = express();
 app.set("view engine", "ejs");
 
-mongoose.connect("mongodb://localhost/secret_app");
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(passport.initialize());
-app.use(passport.session()); 
 app.use(require("express-session") ({
     secret: process.env.SESSIONSECRET,
     resave: false,
     saveUninitialized: false
 }));
 
+mongoose.connect("mongodb://localhost/secret_app");
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(passport.initialize());
+app.use(passport.session()); 
+
+passport.use(new localStrategy(User.authenticate()));
 passport.serializeUser(User.serializeUser());
 passport.deserializeUser(User.deserializeUser());
 
@@ -39,16 +41,27 @@ app.post("/register", function(req, res) {
     if (req.body.password != req.body.confirmpassword) {
         res.render("register", {errorString:"Password does not match"});
     } else {
-        User.register(new User({username: req.body.email}), req.body.password, 
+        User.register(new User({username: req.body.username}), req.body.password, 
         function (error, user) {
             passport.authenticate("local")(req, res, function() {
-                res.redirect("secret");
+                res.redirect("/secret");
             });    
         });
     }
 });
 
-app.get("/secret", function(res, res) {
+app.post("/", passport.authenticate("local", {
+    successRedirect: "/secret",
+    failureRedirect: "/"
+}), function(req, res) {
+} )
+
+app.get("/logout", function(req, res) {
+    req.logout();
+    res.redirect("/");
+});
+
+app.get("/secret", isLoggedIn, function(res, res) {
     res.render("secret")
 });
 
@@ -57,3 +70,10 @@ app.post("/", function(req, res) {
 });
 
 app.listen(process.env.PORT, process.env.IP);
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    res.redirect("/");
+}
